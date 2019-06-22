5dcb7537e616d4803924f807ee90af27a586ecb9
OlhaLu <olhalutsyshyna@gmail.com>
2019-06-22 21:47:57 +0300

    Add disabled on Btn

--- src/js/switch-background.js
+++ src/js/switch-background.js
@@ -2,48 +2,44 @@
 // Напиши скрипт, который после нажатия кнопки Start, раз в секунду меняет цвет фона body на случайное значение
 //  из массива используя инлайн-стиль. При нажатии на кнопку Stop, изменение цвета фона должно останавливаться.
 
-// ⚠️ Учти, на кнопку Start можно нажать бесконечное количество раз. 
+// ⚠️ Учти, на кнопку Start можно нажать бесконечное количество раз.
 // Сделай так, чтобы пока изменение темы запушено, кнопка Start была не активна.
 
-// Для генерации случайного числа (индекс элемента массива цветов), 
+// Для генерации случайного числа (индекс элемента массива цветов),
 // используй функцию randomIntegerFromInterval.
 
 const colors = [
-    '#FFFFFF',
-    '#2196F3',
-    '#4CAF50',
-    '#FF9800',
-    '#009688',
-    '#795548',
-  ];
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
 
-  const body = document.querySelector('body');
-  const startBtn = document.querySelector(".js-start");
-  const stopBtn = document.querySelector(".js-stop");
+const body = document.querySelector('body');
+const startBtn = document.querySelector('.js-start');
+const stopBtn = document.querySelector('.js-stop');
+let interval;
 
-  let interval = null;
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
 
-  const randomIntegerFromInterval = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  };
-
-function chengeBackground(){
-  const colorItem = colors.forEach(el => console.log(el));
-  body.style.background = colorItem;
+function changeBackground(colorsArray) {
+  const randomColorIdx = randomIntegerFromInterval(0, colorsArray.length - 1);
+  body.style.background = colorsArray[randomColorIdx];
 }
 
-  // isActive: false;
-  // if(!isActive)
-  // isActive: true;
-
-
-startBtn.addEventListener("click", () => {
+startBtn.addEventListener('click', () => {
+  startBtn.disabled = true;
   interval = setInterval(() => {
-  console.log('New Color!')
-}, 1000);
+    changeBackground(colors);
+  }, 1000);
 });
 
-stopBtn.addEventListener("click", () => {
+stopBtn.addEventListener('click', () => {
+  startBtn.disabled = false;
   clearInterval(interval);
-  console.log("stopped setting color!");
+  console.log('stopped setting color!');
 });
